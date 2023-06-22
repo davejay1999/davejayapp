ed6696dd6362fbd4b6d04b9c77b39f351fd2cb1d
davejay1999 <davejay1999@gmail.com>
2023-06-21 23:03:33 -0400

    commenting django_heroku

--- backend/settings.py
+++ backend/settings.py
@@ -126,8 +126,8 @@ STATIC_URL = '/static/'
 
 
 # Activate Django-Heroku.
-import django_heroku
+# import django_heroku
 
-django_heroku.settings(locals())
-options = DATABASES['default'].get('OPTIONS', {})
-options.pop('sslmode', None)
+# django_heroku.settings(locals())
+# options = DATABASES['default'].get('OPTIONS', {})
+# options.pop('sslmode', None)
